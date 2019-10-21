05da6f6703a47d24954c0486c9e73d1ec0f936a4
David Isakson <ikeman32@yahoo.com>
2019-10-21 12:31:03 -0700

    Welcome Page

--- foodie/src/App.js
+++ foodie/src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import "./App.css";
+
+import Welcome from './components/welcome/WelcomePage';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FoodApp>
+      <FoodHeader>
+        <FoodH1>FoodieFun</FoodH1>
+        <FoodTag>
+          <p>
+            A place for foodies to share and rate their favorite spots, dishes,
+            and overall dining experience
+          </p>
+        </FoodTag>
+      </FoodHeader>
+      <Welcome />
+    </FoodApp>
   );
 }
+const FoodH1 = styled.h1`
+  @import url("https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap");
+  font-family: "Josefin Sans", sans-serif;
+  color: #ffdd67;
+  font-weight: bold;
+  font-size: 7.2rem;
+`;
+
+const FoodTag = styled.div`
+  width: 50%;
+  font-size: 1.6rem;
+`;
+
+const FoodHeader = styled.header`
+  background-color: #282c34;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: 2.1rem;
+  color: white;
+`;
+
+const FoodApp = styled.div`
+  background-color: #282c34;
+  text-align: center;
+  min-height: 100vh;
+`;
 
 export default App;
--- foodie/src/components/welcome/WelcomePage.js
+++ foodie/src/components/welcome/WelcomePage.js
@@ -1,8 +1,41 @@
-import React from 'react',
-import {styled} from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-const Welcome = () =>{
-
+const Welcome = props => {
+  return (
+    <BtnDiv>
+      <SignIn>Sign In</SignIn>
+      <CreatAcc> Create Account</CreatAcc>
+      <p>Just Browsing -></p>
+    </BtnDiv>
+  );
 };
 
+const BtnDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: white;
+`;
+
+const SignIn = styled.button`
+  background: #ffdd67;
+  border-radius: 4px;
+
+  font-size: 1.6rem;
+  width: 20rem;
+  margin-top: 5rem;
+`;
+
+const CreatAcc = styled.span`
+  background: #313036;
+  border: 1.5px solid #ffdd67;
+  box-sizing: border-box;
+  border-radius: 4px;
+  margin-top: 2rem;
+  padding: 0.5rem;
+  width: 20rem;
+  color: white;
+`;
 export default Welcome;
