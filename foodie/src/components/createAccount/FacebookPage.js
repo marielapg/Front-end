b502dba2129e43ad00777f2c52def9a4bb3354b0
David Isakson <ikeman32@yahoo.com>
2019-10-21 17:31:44 -0700

    Facebook Account Creation update

--- foodie/src/components/createAccount/FacebookPage.js
+++ foodie/src/components/createAccount/FacebookPage.js
@@ -5,22 +5,28 @@ import { BtnDiv } from "../styles/Divs";
 import { Inputs } from "../styles/Forms";
 import { SignInH1 } from "../styles/AppStyles";
 
-const CreateWithFacebook = (props) =>{
-    return(
-        <div>
-        <SignInH1>Found Facebook</SignInH1>
-        <div>
-          <div style={Bg}><p><ProfileIcon/></p> <div style={BtnStyle}>Your Profile</div></div>
+const CreateWithFacebook = props => {
+  return (
+    <div>
+      <SignInH1>Found Facebook</SignInH1>
+      <div>
+        <div style={Bg}>
+          <p>
+            <ProfileIcon />
+          </p>{" "}
+          <div style={BtnStyle}>Your Profile</div>
         </div>
-        <form>
-          <BtnDiv>
-            <Inputs type="password" name="Password" placeholder="Password" />
-  
-            <SignInBtn>Create</SignInBtn>
-          </BtnDiv>
-        </form>
       </div>
-    );
+      <form>
+        <BtnDiv>
+          <Inputs type="password" name="Password" placeholder="Password" />
+
+          <SignInBtn>Create</SignInBtn>
+        </BtnDiv>
+      </form>
+      <p>  Never mind</p>
+    </div>
+  );
 };
 
 export default CreateWithFacebook;
